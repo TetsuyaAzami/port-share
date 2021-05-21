6d573c9d440c329c3b0f220c548a9467c185bb45
Tetsuya Azami <zqianjian35@gmail.com>
2021-05-21 16:37:28 +0900

    test model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-subject(:user){User.new(
-  email: "tetsuya@email.com",
-  password: "password"
-)}
-  context "名前が入力されている場合" do
-  it "ユーザー登録が成功すること" do
-  
-  user.name = "哲也"
-  expect(user).to be_valid
-
+  subject(:user) do
+    User.new(name: '哲也', email: 'tetuya@email.com', password: 'password')
   end
-  end
-
-  context "名前が空の場合" do
-    it "ユーザー登録が成功しないこと" do
-    user.name = ""
-    user.valid?
-    expect(user.errors[:name]).to include("を入力してください。")
-
+  context '名前が入力されている場合' do
+    it 'ユーザー登録が成功すること' do
+      expect(user).to be_valid
     end
   end
 
+  context '名前が空の場合' do
+    it 'ユーザー登録が成功しないこと' do
+      user.name = ''
+      user.valid?
+      expect(user.errors[:name]).to include('を入力してください。')
+    end
+  end
+
+  context 'userが削除された場合' do
+    it '削除されたuserのproductが削除されること' do
+      user.save
+      user.products.create(name: 'ポートフォリオ1', technique: 'Ruby')
+      expect { user.destroy }.to change { Product.count }.by(-1)
+    end
+  end
 end
