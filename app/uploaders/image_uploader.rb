30d20ba7e5888275706fce4811cda8240f7919e3
Tetsuya Azami <zqianjian35@gmail.com>
2021-06-04 05:38:11 +0900

    start-using-cloudinary

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fit: [400, 200]
 
+  include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -18,10 +19,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
+    #   # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    #
+    '/images/fallback/' + [version_name, 'default.png'].compact.join('_')
   end
 
   # Process files as they are uploaded:
@@ -39,7 +40,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w(jpg jpeg gif png)
+    %w[jpg jpeg gif png]
   end
 
   # Override the filename of the uploaded files:
