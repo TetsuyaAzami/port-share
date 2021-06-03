5759a338d26f3dd3d5ef9ce91cdb29421fd62632
Tetsuya Azami <zqianjian35@gmail.com>
2021-06-03 19:34:40 +0900

    modify user_spec.rb

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -1,4 +1,4 @@
 class Technique < ApplicationRecord
-  has_many :product_carts
+  has_many :product_techniques,dependent: :destroy
   has_many :products,through: :product_techniques
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :product do
     name { Faker::Name.name }
     association :user
-    after(:create) { |product| create_list(:technique, 3, products: [product]) }
+    after(:create) { |product| product.product_techniques.create(technique_id: "#{rand(1..15)}") }
   end
 end
