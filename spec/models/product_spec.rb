21681cade07f0c260cea743783ebbeef7f9e3302
Tetsuya Azami <zqianjian35@gmail.com>
2021-06-19 15:54:47 +0900

    add tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,13 +16,38 @@ RSpec.describe Product, type: :model do
   context '名前が空である場合' do
     it 'productが登録されないこと' do
       product.name = ''
-      expect(product).not_to be_valid
+      product.valid?
+      expect(product.errors[:name]).to include("を入力してください。")
     end
   end
 
   context '名前が入力されている場合' do
-    it 'productが登録されないこと' do
+    it 'productの登録に成功すること' do
       expect(user).to be_valid
+    end
+  end
+
+  context '名前が51文字以上である場合' do
+    it '保存に失敗すること' do
+     product.name = "a" * 51
+     product.valid?
+     expect(product.errors[:name]).to include("は50文字以内にしてください。")
+    end
+  end
+
+  context '名前が51文字以上である場合' do
+    it '保存に失敗すること' do
+     product.name = "a" * 51
+     product.valid?
+     expect(product.errors[:name]).to include("は50文字以内にしてください。")
+    end
+  end
+
+  context 'descriptionが251文字以上である場合' do
+    it '保存に失敗すること' do
+     product.description = "a" * 251
+     product.valid?
+     expect(product.errors[:description]).to include("は250文字以内にしてください。")
     end
   end
 
