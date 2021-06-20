6323d4a24cf1c7e18a26e9986f3afe225d528cf0
Tetsuya Azami <zqianjian35@gmail.com>
2021-06-20 16:11:08 +0900

    rename app to port share

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ require 'carrierwave/orm/activerecord'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PortShareWebpacker
+module PortShare
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
