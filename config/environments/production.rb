d0cc64be6e9f3e08007b065113721945bf133490
Tetsuya Azami <zqianjian35@gmail.com>
2021-10-23 20:38:20 +0900

    add config.hosts to server ip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.hosts << 'port-share.herokuapp.com'
+  config.hosts << ENV['SERVER__IP']
 end
