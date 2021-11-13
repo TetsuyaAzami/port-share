edefad1fb995513c39be2d3ab5975f0a1b902b6b
Tetsuya Azami <zqianjian35@gmail.com>
2021-11-13 16:10:59 +0900

    likeモデルの単体テストを追加しました

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:like) { build(:like) }
+
+  describe '正常値と異常値の確認' do
+    context 'likeモデルのバリデーション' do
+      it 'user_idとproduct_idがあれば保存できる' do
+        expect(like).to be_valid
+      end
+
+      it 'user_idがないと保存に失敗すること' do
+        like.user_id = nil
+        like.valid?
+        expect(like.errors[:user_id]).to include('を入力してください')
+      end
+
+      it 'product_idがないと保存に失敗すること' do
+        like.product_id = nil
+        like.valid?
+        expect(like.errors[:product_id]).to include('を入力してください')
+      end
+    end
+  end
 end
